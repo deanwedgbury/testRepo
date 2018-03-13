21031647ac5a228b9fe6768e5006ab2cb3fbe49a
Bluemillers <Bluemillers>
2018-03-13 00:09:13 -0400

    testing database 2

--- code/db/setup.sh
+++ code/db/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-db_user="chanande"
+db_user="csc301app3"
 db_hostname="mcsdb.utm.utoronto.ca"
-db_name="chanande_309"
-db_password="40156"
+db_name="csc301app3"
+db_password="29lnclkmlkn2oi34nc,.g"
 
 sed -e "s/DB_NAME/$db_name/" -e "s/DB_HOSTNAME/$db_hostname/" -e "s/DB_USER/$db_user/" -e "s/DB_PASSWORD/$db_password/" dbconnect_string_template.php > ../lib/dbconnect_string.php
 
