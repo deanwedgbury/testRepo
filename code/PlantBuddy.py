294b7991c2a6def71b7b52c33379e9dd2596dfe6
Bluemillers <Bluemillers>
2018-03-19 23:21:47 -0400

    added old code dir

---
+++ code/PlantBuddy.py
@@ -0,0 +1,72 @@
+# communicate between the pi and the arduino
+
+from subprocess import call
+
+
+
+import time
+
+import smbus
+
+bus = smbus.SMBus(1)
+
+# This is the address we setup in the Arduino Program
+address = 0x04
+
+def writeNumber(value):
+    bus.write_byte(address, value)
+    return -1
+
+def readNumber():
+    number = bus.read_byte(address)
+    return number
+
+'''
+while True:
+    var = input("Enter 1 for temp(in celsius), 2 for humdity(0-100%), 3 for moisture(0-255) or 9 to toggle the motor: ")
+    if not var:
+        continue
+
+    writeNumber(var)
+    # sleep one second
+    time.sleep(1)
+
+    number = readNumber() # this is the value the arduino responds with
+    print "Arduino: ", number
+    print
+'''
+
+def readMoisture():
+    
+    # Test Get only moisture
+    writeNumber(3)
+    # sleep one second
+    time.sleep(1)
+    number = readNumber() # this is the value the arduino responds with
+    
+    #number = 1
+    return number
+
+if __name__ == '__main__':
+    interval = 1#10
+    i=0
+    while True:
+        time.sleep(interval)
+        
+        # Get values from arduino
+        moisture=readMoisture()
+
+        # Update the database
+        # Read from database and send 9 to arduino if water=True
+        #call(["ls", "-l"])
+        call(["java", "Update"])
+
+        i=i+1
+
+
+    
+
+
+
+
+
---
+++ code/update.sql
@@ -0,0 +1,1 @@
+INSERT INTO user(username, password, name) VALUES ("aaa", "aaa", "aaa");
